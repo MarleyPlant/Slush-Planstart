0b026f84d70a596256de25836b67351c1118bfdd
Marley Plant <marley@marleyplant.com>
2017-03-21 22:27:01 +0000

    Finish Bootstrap supporrt

---
+++ templates/functions.php
@@ -0,0 +1,16 @@
+<?php
+function <%= appNameSlug %>_load_js(){ //Load the Js Librararies needed
+  // Register the script like this for a theme:
+  wp_register_script( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap-min.js'); //Load generated Bootstrap_JS
+
+  wp_enqueue_script( 'bootstrap' ); //Add bootstrap_JS to header
+}
+
+function <%= appNameSlug %>_load_css(){
+  wp_register_style('bootstrap', get_template_directory_uri() . '/assets/bootstrap.css'); //Load generated bootstrap
+
+  wp_enqueue_style('bootstrap'); //Add bootstrap to header
+}
+add_action( 'wp_enqueue_scripts', '<%= appNameSlug %>_load_css'); //Load the CSS Libraries into the header.
+add_action( 'wp_enqueue_scripts', '<%= appNameSlug %>_load_js' ); //Load the JS Libraries into the header.
+?>
---
+++ templates/header.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title><%= appName %></title>
+    <?php wp_head(); ?>
+  </head>
+
+  <body>
+    <div id="page-content">
+      
